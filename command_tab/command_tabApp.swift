a635515df971a992cd06e3a9a97ea66c531df472
tal <nexus0x42@gmail.com>
2026-01-19 19:46:42 -1000

    Initial Commit

---
+++ command_tab/command_tabApp.swift
@@ -0,0 +1,17 @@
+//
+//  command_tabApp.swift
+//  command_tab
+//
+//  Created by tal on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct command_tabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
